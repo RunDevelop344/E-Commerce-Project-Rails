1dc79494ee7feb551a85b091441157d9882b7047
Ssimran-5 <simranbhumbak22@gmail.com>
2026-03-26 09:00:14 -0500

    assigned role to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  def change
-    add_column :users, :role, :string, default: "customer"
-  end
-end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
