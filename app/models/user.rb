6abb09535404487ba68460e1d95344bef34b1aaf
Ssimran-5 <simranbhumbak22@gmail.com>
2026-03-26 11:04:45 -0500

    modified manifest.js

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
 
+  def admin?
+  role == "admin"
+end
+
+def customer?
+  role == "customer"
+end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
