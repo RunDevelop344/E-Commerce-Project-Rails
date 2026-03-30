595231bd72321735e644dbc45dfb3485401ffc19
Ssimran-5 <simranbhumbak22@gmail.com>
2026-03-29 22:22:31 -0500

    updated controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,49 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to @order, notice: "Order created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @order.update(order_params)
+      redirect_to @order, notice: "Order updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @order.destroy
+    redirect_to orders_path, notice: "Order deleted successfully."
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:customer_id, :status, :total_price)
+  end
 end
